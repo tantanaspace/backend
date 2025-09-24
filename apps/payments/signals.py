b9bf44fe89bbc7b5fbdd940fc2074517ac3423ba
Javohir Nuftillayev <nuftillayev@aKOs-MacBook-Pro-Home.local>
2025-09-24 20:21:28 +0500

    update: payment tasks

--- apps/payments/signals.py
+++ apps/payments/signals.py
@@ -2,11 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.payments.models import PaymentTransaction
-from apps.payments.tasks import send_payment_successful_notification
 
 
 @receiver(post_save, sender=PaymentTransaction)
 def payment_successful(sender, instance, **kwargs):
-    if instance.status == PaymentTransaction.StatusType.ACCEPTED and instance.is_notification_sent is False:
+    if instance.status == PaymentTransaction.StatusType.ACCEPTED:
         pass
-        # send_payment_successful_notification.delay(instance.id)
+        # todo: send notification to user
